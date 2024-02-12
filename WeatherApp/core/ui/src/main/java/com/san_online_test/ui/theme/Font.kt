cbf6f15990352c167b289e27cee549d6ad03e104
Ilya Krasovickij <145861478+IlyaKrasovickij03@users.noreply.github.com>
2024-02-12 10:44:59 +0300

    Добавлены шрифты, настройка typography

---
+++ WeatherApp/core/ui/src/main/java/com/san_online_test/ui/theme/Font.kt
@@ -0,0 +1,11 @@
+package com.san_online_test.ui.theme
+
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import com.san_online_test.ui.R
+
+val tommySoftFontFamily = FontFamily(
+    Font(R.font.tommy_soft_bold, FontWeight.Bold),
+    Font(R.font.tommy_soft_regular, FontWeight.Normal),
+)
