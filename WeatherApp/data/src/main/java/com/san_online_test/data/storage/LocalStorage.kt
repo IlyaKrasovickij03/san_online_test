1a638aea3853faf250af17971e884f80364627bf
Ilya Krasovickij <145861478+IlyaKrasovickij03@users.noreply.github.com>
2024-02-15 12:00:07 +0300

    Реализован local storage

---
+++ WeatherApp/data/src/main/java/com/san_online_test/data/storage/LocalStorage.kt
@@ -0,0 +1,42 @@
+package com.san_online_test.data.storage
+
+import android.util.Log
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.stringPreferencesKey
+import com.san_online_test.data.location.DefaultLocationTracker
+import com.san_online_test.domain.model.Location
+import kotlinx.coroutines.flow.first
+
+private const val LATITUDE_KEY = "latitudeKey"
+private const val LONGITUDE_KEY = "longitudeKey"
+
+class LocalStorage(
+    private val localDataBase: DataStore<Preferences>,
+    ) {
+
+    private val lastUserLatitude: Preferences.Key<String> = stringPreferencesKey(LATITUDE_KEY)
+    private val lastUserLongitude: Preferences.Key<String> = stringPreferencesKey(LONGITUDE_KEY)
+
+    suspend fun saveLastUserLocation(location: Location) {
+        val latitude = location.latitude.toString()
+        val longitude = location.longitude.toString()
+        Log.d("LATITUDE_IN_STORAGE", latitude)
+        Log.d("LONGITUDE_IN_STORAGE", longitude)
+        localDataBase.edit {
+            it[lastUserLatitude] = latitude
+            it[lastUserLongitude] = longitude
+            Log.d("LATITUDE_SAVED_IN_STORAGE", it[lastUserLatitude]!!)
+            Log.d("LONGITUDE_SAVED_IN_STORAGE", it[lastUserLongitude]!!)
+        }
+    }
+
+    suspend fun getLastUserLocation(): Location {
+        val lastUserLatitude = localDataBase.data.first()[lastUserLatitude]
+        val lastUserLongitude = localDataBase.data.first()[lastUserLongitude]
+        return if (lastUserLatitude == null && lastUserLongitude == null) Location(55.7522, 37.6156)
+        else Location(latitude = lastUserLatitude!!.toDouble(), longitude = lastUserLongitude!!.toDouble())
+    }
+
+}
