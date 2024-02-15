2b62e0c07981632cefe477c2c0c88449c516d485
Ilya Krasovickij <145861478+IlyaKrasovickij03@users.noreply.github.com>
2024-02-15 12:02:18 +0300

    Реализовано получение локации пользователя, а также дефолтной локации и сохранение последней локации в local storage

--- WeatherApp/app/src/main/java/com/san_online_test/weatherapp/app/App.kt
+++ WeatherApp/app/src/main/java/com/san_online_test/weatherapp/app/App.kt
@@ -1,6 +1,22 @@
 package com.san_online_test.weatherapp.app
 
 import android.app.Application
+import com.san_online_test.weatherapp.di.AppModule
+import com.san_online_test.weatherapp.di.DaggerAppComponent
+import com.san_online_test.weatherapp.di.DiProvider
 
 class App: Application() {
+    override fun onCreate() {
+        super.onCreate()
+        setupDagger()
+    }
+
+
+    private fun setupDagger(){
+        val appComponent = DaggerAppComponent
+            .builder()
+            .appModule(AppModule(application = this, context = applicationContext))
+            .build()
+        DiProvider.appComponent = appComponent
+    }
 }
